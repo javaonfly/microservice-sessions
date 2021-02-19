b0be34caf1efb8501707a62b27fdd3ad3098813d
swastika <swastika.basu@gmail.com>
2021-02-19 12:13:17 +0530

    Cleanup

--- day3-EmployeeCQRS/src/main/java/org/javaonfly/cqrs/Employee/repo/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.javaonfly.cqrs.Employee.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
